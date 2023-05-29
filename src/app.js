013861b965a27c3925d7ca6615d0d63c2dedf005
Pablo García de Gregorio <129775284+pablogarciadegregorio@users.noreply.github.com>
2023-05-29 12:19:22 +0200

    Ejercicio ++.js

--- src/app.js
+++ src/app.js
@@ -7,18 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let pronoun = ['the','our'];
+let pronoun = ['the','our'];
 var adj = ['great', 'big' ];
-var noun = ['jogger','racoon'];
+var noun = ['jogger','racoon', 'renaultclio', 'notaxes', 'sitcom', 'fishnet' ];
 var domain = ['.com','.es', '.net', '.io'];
-
 
 for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
         for (let l = 0; l < domain.length; l++) {
-          console.log(pronoun[i] + adj[j] + noun[k]+ domain[l]);
+        //   console.log(pronoun[i] + adj[j] + noun[k]+ domain[l]);
+            
+            // DOMAIN SIN EL PUNTO
+            var compDomain = domain[l].substring(1,domain.length);
 
+            // NOUN SOLO CON EL DOMINIO
+            var compNoun = noun[k].substring((noun[k].length - (domain[l].length ) + 1), noun[k].length+1);
+
+            if (compDomain == compNoun) {
+                console.log(noun[k].substring(0, noun[k].length - domain[l].length +1) + domain[l]);
+                
+            }
+            else{
+                console.log(pronoun[i] + adj[j] + noun[k]+ domain[l]);
+            }
 
         }
 
@@ -31,4 +43,16 @@ for (let i = 0; i < pronoun.length; i++) {
 
 
 
-};
+// DOMAIN SIN EL PUNTO
+var compDomain = domain[2].substring(1,domain.length);
+
+// NOUN SOLO CON EL DOMINIO
+var compNoun = noun[5].substring((noun[5].length - (domain[2].length ) + 1), noun[5].length+1);
+
+if (compDomain = compNoun) {
+    console.log(noun[5].substring(0, noun[5].length - domain[2].length +1) + domain[2]);
+    
+}
+
+
+
